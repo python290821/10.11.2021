232429a91a77a3c27f0b5eb139b5b12dfdb31129
python290821 <90146835+python290821@users.noreply.github.com>
2021-11-10 20:40:42 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,3 +1,4 @@
+import math
 import random
 class Person:
     def __init__(self, id=0, name='Incognito'):
@@ -29,16 +30,34 @@ class Rectangle:
     def calc_area(self):
         return self.width * self.height
 
+    def calc_hekef(self):
+        return 2 * abs(self.width) + 2 * abs(self.height)
+
     def __str__(self):
         return f'Rectangle (width: {self.width}, height: {self.height}, '+\
-               f'area: {self.calc_area()})'
+               f'area: {self.calc_area()}, hekef: {self.calc_hekef()})'
 
 rec1 = Rectangle(3.2, 7.88)
 print(rec1)
-#print('area =',calc_area(rec1.width, rec1.height))
 print('area =',rec1.calc_area())
+print('hekef =',rec1.calc_hekef())
 rec2 = Rectangle(9, 12)
 print(rec2)
 
+class Myname:
 
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+
+    def __len__(self):
+        return len(self.fname) + len(self.lname)
+
+    def __str__(self):
+        return f'Myname (fname: "{self.fname}", lname: "{self.lname}", '\
+               f'len: {self.__len__()})';
+
+danny = Myname('danny','cohen')
+print(danny)
+print('danny len = ', len(danny))
 
