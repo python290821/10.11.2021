5472a45484a623c35840b6418e6c6e678465ba58
python290821 <90146835+python290821@users.noreply.github.com>
2021-11-10 18:34:25 +0200

    Update program.py

--- program.py
+++ program.py
@@ -1,11 +1,16 @@
 class Book():
-    pass
+    # constructor
+    def __init__(self, title, author, year_of_publish, genre, no_of_pages):
+        # self = Book()
+        self.title = title
+        self.author = author
+        self.year_of_publish = year_of_publish
+        self.genre = genre
+        self.number_of_pages = no_of_pages
+        # return self
 
-def create_book(title, author, year_of_publish, genre, no_of_pages):
-    book = Book()
-    book.title = title
-    book.author = author
-    book.year_of_publish = year_of_publish
-    book.genre = genre
-    book.number_of_pages = no_of_pages
-    return book
+cooking_book = Book('cooking in 1 hour', 'rut', 2020, 'food', 155)
+racing_car = Book('formula 1', 'john', 2022, 'fast', 300)
+
+print(cooking_book.__dict__)
+
